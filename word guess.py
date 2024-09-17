97de1f9b30fe034ccd37641072e37f1b58dc71f5
DEEPAK-534 <deepakorugonda@gmail.com>
2024-09-17 20:08:02 +0530

    Add files via upload

---
+++ word guess.py
@@ -0,0 +1,36 @@
+import time
+import random
+name = input("What is your name? ")
+print("Good Luck ! ", name)
+time.sleep(1)
+print("Start guessing...\n")
+time.sleep(0.5)
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+word = random.choice(words)
+print("Guess the characters")
+guesses = ''
+turns = 12
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char,end=" ")
+        else:
+            print("_",end=" ")
+            failed += 1
+    print()
+    if failed == 0:
+        print("You Win")
+        print("The word is: ", word)
+        break
+    print()
+    guess = input("guess a character:")
+    guesses += guess
+    if guess not in word:
+        turns -= 1
+        print("Wrong")
+        print("You have", + turns, 'more guesses')
+        if turns == 0:
+            print("You Loose")
